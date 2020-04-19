0da841e03b8de70738a774b02cf12b971b2d4e99
Abdullah <azravian@gmail.com>
2020-04-20 01:32:00 +0300

    Modification made

--- Assignment1_19042020/Assignment1_19042020.py
+++ Assignment1_19042020/Assignment1_19042020.py
@@ -11,24 +11,20 @@ Number2=int(input("Enter Second Number: "))
 #addintion of two numbers
 sum_result = Number1 + Number2
 print(f"sum is of {Number1} and {Number2} is {sum_result}")
-del sum_result   #del variable, not required for this code, but a good practice to do so
 
 
 #subtraction of two numbers
 sub_result = Number1 - Number2
 print(f"The result of {Number1} - {Number2} is {sub_result}")
-del sub_result   #del variable 
 
 #Division of two numbers
 div_result = Number1 / Number2
 mod_result = Number1 % Number2 
 print(f"The result of {Number1} divided by {Number2} is {div_result}")
-del div_result   #del variable 
 
 #Multiplication of two numbers
 mult_result = Number1 * Number2
 print(f"The result of {Number1} multiplied by {Number2} is {mult_result}")
-del mult_result   #del variable 
 
 
 '''
